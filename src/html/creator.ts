f2dc860a8b499a03042ab8106d188a06e6c71b6e
Kyu0 <efftar@naver.com>
2025-04-29 22:51:35 +0900

    docs: 클래스, 함수 주석 추가

--- src/html/creator.ts
+++ src/html/creator.ts
@@ -3,9 +3,18 @@ type CreateOptions = {
   position?: 'left' | 'right' | 'none';
 };
 
+/**
+ * 익스텐션에서 추가로 사용할 HTML 태그를 생성하는 클래스입니다.
+ */
 export class CustomHTMLCreator {
   constructor() {}
 
+  /**
+   * 사용자 정의 HTML Button 태그를 생성합니다.
+   *
+   * @param options 생성할 버튼의 옵션
+   * @returns 생성된 HTML Button 태그
+   */
   public createButton(options: CreateOptions) {
     const span = document.createElement('span');
     span.innerText = options.content;
--- src/html/util.ts
+++ src/html/util.ts
@@ -1,3 +1,9 @@
+/**
+ * 특정 HTML 태그의 클래스 목록을 복사하여 다른 HTML 태그의 클래스 목록으로 붙여넣습니다.
+ *
+ * @param from 복사할 HTML 태그
+ * @param to 복사한 클래스 목록을 붙여넣을 HTML 태그
+ */
 export function copyClassList(
   from: HTMLElement | undefined,
   to: HTMLElement
