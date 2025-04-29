56db2c039446ba3b6b7db6f2d92fe27dd057099b
Kyu0 <efftar@naver.com>
2025-04-29 23:23:07 +0900

    feat: 템플릿 저장하기, 불러오기 버튼 이벤트 리스너 추가

--- src/html/parser.ts
+++ src/html/parser.ts
@@ -1,19 +1,23 @@
 /**
- * '작성하기' 페이지에 존재하는 HTML 태그를 추출하여 제공하는 클래스입니다.
+ * '작성하기' 페이지에 존재하는 HTML 태그를 추출하여 태그 혹은 필요한 태그의 값을 제공하는 클래스입니다.
  *
  *  - exitButton: 나가기 버튼
  *  - publishButton: 출간하기 버튼
+ *  - contentSpan: 작성 중인 포스트 내용
  */
 export class Parser {
   private readonly _exitButton;
   private readonly _publishButton;
+  private readonly _contentSpan;
   private _isContinuable = false;
 
   private static readonly EXIT_BUTTON_TEXT = '나가기';
   private static readonly PUBLISH_BUTTON_TEXT = '출간하기';
+  private static readonly CONTENT_SPAN_ROLE = 'presentation';
 
   constructor() {
     const buttons = Array.from(document.querySelectorAll('button'));
+    const spans = Array.from(document.querySelectorAll('span'));
 
     this._exitButton = buttons.find(
       (button) => button.innerText === Parser.EXIT_BUTTON_TEXT
@@ -23,7 +27,15 @@ export class Parser {
       (button) => button.innerText === Parser.PUBLISH_BUTTON_TEXT
     );
 
-    if (this._exitButton != null && this._publishButton != null) {
+    this._contentSpan = spans.find(
+      (span) => span.role === Parser.CONTENT_SPAN_ROLE
+    );
+
+    if (
+      this._exitButton != null &&
+      this._publishButton != null &&
+      this._contentSpan != null
+    ) {
       this._isContinuable = true;
     }
   }
@@ -34,6 +46,18 @@ export class Parser {
 
   get publishButton(): HTMLButtonElement | undefined {
     return this._publishButton;
+  }
+
+  get contentSpan(): HTMLSpanElement | undefined {
+    return this._contentSpan;
+  }
+
+  get writingContent(): string | undefined {
+    const contentSpan = Array.from(document.querySelectorAll('span')).find(
+      (span) => span.role === Parser.CONTENT_SPAN_ROLE
+    );
+
+    return contentSpan?.innerText;
   }
 
   get isContinuable(): boolean {
